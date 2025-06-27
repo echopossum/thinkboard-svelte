ba61ed855c3589584654664324c0bb95cf4768c4
Echo <echopossum@gmail.com>
2025-06-27 00:27:18 -0400

    Refactor create note to use form actions

---
+++ src/routes/create/+page.server.js
@@ -0,0 +1,20 @@
+import Note from '$lib/models/Note.js';
+import { json, redirect } from '@sveltejs/kit';
+
+export const actions = {
+	create: async ({ request }) => {
+		try {
+			const data = await request.formData();
+			const title = data.get('title');
+			const content = data.get('content');
+			const note = new Note({ title, content });
+			const savedNote = await note.save();
+			console.log('Redirecting');
+			throw redirect(303, '/');
+		} catch (error) {
+			if (error instanceof Response || error.status === 303) {
+				throw error;
+			}
+		}
+	}
+};
